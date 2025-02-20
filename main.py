a0f3919b143e33a49bb3f5af75d7d0701f74be56
garyPenhook <penhookcoder@gmail.com>
2025-02-20 11:58:49 -0500

    Initial commit with .gitignore update

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 # source venv/bin/activate
 
-import math
+def main():
+    print("Hello World")
 
-
-class Dog:
-    def __init__(self, breed):
-        self.breed = breed
-
-
-mutt = Dog("Mutt")
-poodle = Dog("Poodle")
-
-print(mutt.breed)
-print(poodle.breed)
+if __name__ == "__main__":
+    main()
